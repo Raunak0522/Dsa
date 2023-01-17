a62d05ea982ffa434defae878332dd6d852173b9
Raunak0522 <raunakakrarsh@gmail.com>
2023-01-17 12:03:29 +0530

    add

--- Demo.java
+++ Demo.java
@@ -1,16 +1,33 @@
-import java.util.Scanner;
+// import java.util.Scanner;
 
+// public class Demo{
+//      public static void main(String[] args) {
+//         Scanner scn=new Scanner(System.in);
+//         int n= scn.nextInt();
+//         int a=0;
+//         int b=1;
+//         for(int i=1;i<=n;i++){
+//                 System.out.println(a);
+//                 int c=a+b;
+//                 a=b;
+//                 b=c;
+//         }
+//     }
+// }
+
+
+//Count no digits
+import java.util.*;
 public class Demo{
-     public static void main(String[] args) {
+    public static void main(String[] args){
         Scanner scn=new Scanner(System.in);
-        int n= scn.nextInt();
-        int a=0;
-        int b=1;
-        for(int i=1;i<=n;i++){
-                System.out.println(a);
-                int c=a+b;
-                a=b;
-                b=c;
-        }
+            int n=scn.nextInt();
+            int count=0;
+            while(n!=0){
+                n=n/10;
+                count++;
+            }
+         System.out.print(count);
+        
     }
 }
